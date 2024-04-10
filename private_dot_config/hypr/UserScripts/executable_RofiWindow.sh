26dfadd74ef1c84274320add0e5c9e908d0502ee
Przemyslaw Swierczek <swierq@gmail.com>
2024-04-10 21:28:50 +0200

    wofi to rofi

--- private_dot_config/hypr/UserScripts/executable_RofiWindow.sh
+++ private_dot_config/hypr/UserScripts/executable_RofiWindow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WINDOW=$(hyprctl clients | grep "class: " | awk '{gsub("class: ", "");print}' | wofi --show dmenu)
+WINDOW=$(hyprctl clients | grep "class: " | awk '{gsub("class: ", "");print}' | rofi -dmenu -config ~/.config/rofi/config-compact.rasi -i -p "")
 if [ "$WINDOW" = "" ]; then
     exit
 fi
