969061eb67ca4b42041e3538a3f20a7cabc590e6
Przemyslaw Swierczek <swierq@gmail.com>
2024-04-10 21:37:06 +0200

    rofi float

--- private_dot_config/hypr/UserScripts/executable_RofiWindow.sh
+++ private_dot_config/hypr/UserScripts/executable_RofiWindow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WINDOW=$(hyprctl clients | grep "class: " | awk '{gsub("class: ", "");print}' | rofi -dmenu -config ~/.config/rofi/config-compact.rasi -i -p "")
+WINDOW=$(hyprctl clients | grep "class: " | awk '{gsub("class: ", "");print}' | rofi -normal-window -dmenu -config ~/.config/rofi/config-compact.rasi -i -p "")
 if [ "$WINDOW" = "" ]; then
     exit
 fi
