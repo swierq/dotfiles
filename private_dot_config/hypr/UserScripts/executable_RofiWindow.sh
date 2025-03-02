36c1e43f5ed73b978129aade58bc842f85f01617
Przemyslaw Swierczek <swierq@gmail.com>
2025-03-02 17:35:14 +0100

    hyprland cleanup and changes

--- private_dot_config/hypr/UserScripts/executable_RofiWindow.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WINDOW=$(hyprctl clients | grep "class: " | awk '{gsub("class: ", "");print}' | rofi -normal-window -dmenu -config ~/.config/rofi/config-compact.rasi -i -p "")
-if [ "$WINDOW" = "" ]; then
-    exit
-fi
-hyprctl dispatch focuswindow $WINDOW
